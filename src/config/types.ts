6d5c6a79f70e8197ba897b027a07204820fcf3c3
Kristaps Kalnins <kristaps@tribeca.so>
2022-01-10 17:04:32 +0200

    Adds minter settings for several DAOs

--- src/config/types.ts
+++ src/config/types.ts
@@ -7,6 +7,22 @@ export interface GovernorConfig {
   address: string;
   govTokenMint: string;
   customLogoURI?: string;
+  /**
+   * Settings for minting tokens as the DAO. Enabling this allows DAO members to create "mint" proposals which can be used for grants.
+   */
+  minter?: {
+    /**
+     * The Quarry mint wrapper.
+     */
+    mintWrapper: string;
+    /**
+     * The redeemer key, if applicable.
+     *
+     * The consumer of the redeemer key should be aware of how to handle different types of redeemers,
+     * based on the owner of this account.
+     */
+    redeemer?: string;
+  };
   gauge?: {
     /**
      * The Gaugemeister, if gauges are enabled for this governor.
