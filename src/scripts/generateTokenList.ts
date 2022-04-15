1fc3a0ef87c028a69c080435c247bbe21e1b0a70
Surya Khosla <surya@sunny.ag>
2022-04-15 16:42:47 -0500

    Update token list

--- src/scripts/generateTokenList.ts
+++ src/scripts/generateTokenList.ts
@@ -1,13 +1,16 @@
-import { GitHubTokenListResolutionStrategy } from "@solana/spl-token-registry";
+import type { TokenList } from "@saberhq/token-utils";
+import axios from "axios";
 import * as fs from "fs/promises";
 
 import { stableStringify } from "../utils/serialize";
+
+const TOKEN_LIST = `https://github.com/CLBExchange/certified-token-list/blob/master/token-list.json?raw=true`;
 
 /**
  * Fetches the token list from GitHub.
  */
 const fetchTokens = async (): Promise<void> => {
-  const allTokens = await new GitHubTokenListResolutionStrategy().resolve();
+  const { data: allTokens } = await axios.get<TokenList>(TOKEN_LIST);
   await fs.writeFile(
     `${__dirname}/../../solana-token-list.json`,
     stableStringify(allTokens)
