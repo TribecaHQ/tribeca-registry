a5c71af68f1d88e844b3ba22b9ecd77200bfd290
Surya Khosla <surya@sunny.ag>
2022-04-15 16:55:26 -0500

    token list cleanup

--- src/utils/getTokenInfo.ts
+++ src/utils/getTokenInfo.ts
@@ -3,18 +3,27 @@ import type { TokenInfo, TokenList } from "@saberhq/token-utils";
 import { networkToChainId } from "@saberhq/token-utils";
 import * as fs from "fs/promises";
 
+let cachedTokenList: TokenList | null = null;
+
+export const loadTokenList = async (): Promise<TokenList> => {
+  if (cachedTokenList) {
+    return cachedTokenList;
+  }
+  const tokenListRaw = await fs.readFile(
+    `${__dirname}/../../solana-token-list.json`
+  );
+  cachedTokenList = JSON.parse(tokenListRaw.toString()) as TokenList;
+  return cachedTokenList;
+};
+
 export const getTokenInfo = async (
   address: string,
   network?: Network
 ): Promise<TokenInfo | undefined> => {
-  const tokenListRaw = await fs.readFile(
-    `${__dirname}/../../solana-token-list.json`
-  );
-  const { tokens } = JSON.parse(tokenListRaw.toString()) as TokenList;
+  const { tokens } = await loadTokenList();
   if (!network) {
     return tokens.find((tok) => tok.address === address);
   }
-
   const chainId = networkToChainId(network);
   return tokens.find(
     (tok) => tok.chainId === chainId && tok.address === address
